ef1c3dc9188a8ac63c3a34494ceb6f46c7870578
Willianfreitas01 <willian.chris.89@gmail.com>
2025-03-26 22:26:12 -0300

    feat: bot respond to messages (o cara faz magica)

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-/* aqui é onde o filho chora e a mãe n ve */
+
+const axios = require("axios");
+const { Telegraf } = require("telegraf");
+require("dotenv").config();
+
+
+const express = require("express");
+const app = express();
+
+
+app.use(express.json());app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+
+  const bot = new Telegraf(process.env.BOT_TOKEN);
+  const WEBHOOK_URL = process.env.WEBHOOK_TOKEN_URL;
+  const TELEGRAM_API = `https://api.telegram.org/bot${process.env.BOT_TOKEN}`;
+
+  const setWebhook = async () => {
+    try {
+      const response = await axios.post(
+        `${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+
+
+  bot.on("text", (ctx) => {
+    if (ctx.message.text === ".") {
+      ctx.reply(`vai se foder ${ctx.message.from.first_name}, sua bixa!`);
+    }
+  });
+
+
+
+  bot.launch();
+
+  app.listen(3000, async () => {
+    console.log("Server is running on port 3000");
+    await setWebhook();
+  });
---
+++ src/functions.js
@@ -0,0 +1,1 @@
+/* aqui faço minha magia */
